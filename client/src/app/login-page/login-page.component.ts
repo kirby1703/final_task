3b800b5dd621383d6d6c9029554c036d302d19be
Kirill B <sharapov669@gmail.com>
2022-03-27 12:26:42 +0300

    master

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../shared/layouts/services/auth.service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  form!: FormGroup;
+  aSub!: Subscription
+
+  constructor(private auth: AuthService,
+              private router: Router,
+              private route: ActivatedRoute) {
+    
+   }
+
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe()         
+    }
+   }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']){
+        // Registration Successeful. User can log in to created account
+      } else if (params['accessDenied']) {
+        // Authorization required
+      }
+    }) 
+  }
+
+  onSubmit() {
+    this.form.disable()
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => this.router.navigate(['/overview']),
+      error => {
+        console.warn(error)
+        this.form.enable()
+      }
+    )
   }
 
 }
